c7e5d98ce3b37c18772ef0ab7230d6404063a362
LucasIsac <lisaacdlf21@gmail.com>
2025-07-12 10:40:36 -0300

    Agregar página y formulario de recuperación de contraseña con estilos correspondientes

--- frontend/src/pages/Recuperar.jsx
+++ frontend/src/pages/Recuperar.jsx
@@ -1,12 +1,16 @@
 // src/pages/Recuperar.jsx
+import Header from '../components/Header';
+import RecuperarForm from '../components/RecuperarForm';
+import '../assets/styles/recuperar.css';
+
 function Recuperar() {
   return (
-    <div className="login-container">
-      <div className="login-form">
-        <h2>Recuperar contraseña</h2>
-        <p>Próximamente vas a poder restablecer tu contraseña desde aquí.</p>
+    <>
+      <Header />
+      <div className="recuperar-container">
+        <RecuperarForm />
       </div>
-    </div>
+    </>
   );
 }
 
